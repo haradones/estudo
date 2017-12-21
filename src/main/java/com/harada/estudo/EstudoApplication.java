56471019fe7648c72ca351210b7b33a0ba75cbe6
Fernando Harada <fernando.harada@gmail.com>
2017-12-21 12:11:34 -0200

    Criando operacao de instanciacao

--- src/main/java/com/harada/estudo/EstudoApplication.java
+++ src/main/java/com/harada/estudo/EstudoApplication.java
@@ -1,12 +1,30 @@
 package com.harada.estudo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.harada.estudo.domain.Categoria;
+import com.harada.estudo.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class EstudoApplication {
+public class EstudoApplication implements CommandLineRunner {
+
+  @Autowired
+  private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoApplication.class, args);
 	}
+
+  @Override
+  public void run(String... arg0) throws Exception {
+    Categoria cat1 = new Categoria(null, "informatica");
+    Categoria cat2 = new Categoria(null, "escritorio");
+
+    categoriaRepository.save(Arrays.asList(cat1, cat2));
+  }
 }
